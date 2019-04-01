7b6fb3ae26f1da4f381db965cf07d3ff96db2b14
Fernando <fernando_lopezc@live.com>
2019-04-01 10:32:40 +0100

    Done.

--- application.rb
+++ application.rb
@@ -1,10 +1,15 @@
 class Application
+# this method is here to return a response which constists of the status code, any headers m and the body using the Rack::Response object
+# going to create a basic webApp .
 
-  def call(env)
-    resp = Rack::Response.new
-    resp.write "Hello, World"
-    resp.finish
-  end
+def call(env)
+  resp = Rack::Response.new
+  # adding some text to the body 
+  resp.write "Hello, World" 
+  # completin the response wit hthe finish method
+  resp.finish   
+  # by default rack sets m status codes and headers. the "env" input will hold all of the request info and will e used later 
+end
 
 end
 
--- config.ru
+++ config.ru
@@ -1,3 +1,8 @@
 require_relative "./application.rb"
 
+# in this vile i have set up a HTTP webserver that will will reecive the request, 
+
 run Application.new
+
+# hen i run rackup config.ru in the terminal I should get some info , look at the port it will y port = 9292 
+# type in http://localhost:9292/ nd i will see hellow world 
